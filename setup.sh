77f85b3fb2fb700b763dc01206b04f271e995889
Davey Struijk <mail@daveystruijk.com>
2017-06-12 14:59:07 +0200

    Run GUI logic only when a GUI is detected

--- setup.sh
+++ setup.sh
@@ -1,35 +1,38 @@
 #!/bin/sh
 
+if ! xset q &>/dev/null; then
+    HAS_GUI = 1
+fi
+
+# Set timezone
 sudo timedatectl set-timezone Europe/Amsterdam
 
 # Binaries
 sudo apt-get -y install open-vm-tools open-vm-tools-desktop git vim tmux zsh dconf-tools dconf-cli silversearcher-ag
-
-dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 
 # Vim
 if [ ! -d "$HOME/.vim_runtime" ]; then
     git clone https://github.com/amix/vimrc.git ~/.vim_runtime
     sh ~/.vim_runtime/install_awesome_vimrc.sh
 fi
-if [ ! -f "$HOME/.vim_runtime/my_configs.vim" ]; then
-    cp my_configs.vim $HOME/.vim_runtime/my_configs.vim
-fi
-
-# Tmux
-if [ ! -f "$HOME/.tmux.conf" ]; then
-    cp tmux.conf $HOME/.tmux.conf
-fi
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-# Zsh
-cp zshrc $HOME/.zshrc
+# Link dotfiles
+ln -srf my_configs.vim $HOME/.vim_runtime/my_configs.vim
+ln -srf tmux.conf $HOME/.tmux.conf
+ln -srf zshrc $HOME/.zshrc
 
 # Git
 git config --global user.email "mail@daveystruijk.com"
 git config --global user.name "Davey Struijk"
 
-# Colorscheme
-git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ./gnome-terminal-colors-solarized
-bash -c "./gnome-terminal-colors-solarized/install.sh -s dark -p Default --install-dircolors"
+if [ -n "$HAS_GUI" ]; then
+    # Swap caps lock & escape keys
+    dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
+
+    # Colorscheme
+    git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ./gnome-terminal-colors-solarized
+    bash -c "./gnome-terminal-colors-solarized/install.sh -s dark -p Default --install-dircolors"
+fi
+
