fc9ee0842f51904969c726776c94491c19c6a6b9
Davey Struijk <mail@daveystruijk.com>
2024-03-03 15:19:56 +0100

    Replace null_ls with nvim-lint and conform

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,3 @@
------------------------------------------------------------
--- Plugins
------------------------------------------------------------
-
 function border(hl_name)
 	return {
 		{ "╭", hl_name },
@@ -15,43 +11,79 @@ function border(hl_name)
 	}
 end
 
-require("packer").startup(function(use)
-	-- Essentials
-	use("wbthomason/packer.nvim")
-	use("christoomey/vim-tmux-navigator")
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
+-------------------------------------------------------------------------------
+-- Options
+-------------------------------------------------------------------------------
 
-	-- Git
-	use("tpope/vim-fugitive")
-	use({
+vim.opt.completeopt = "menu,menuone,noselect"
+vim.opt.number = true -- enable line numbers
+vim.opt.signcolumn = "number"
+vim.opt.showmatch = true
+vim.opt.undofile = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.scrolloff = 4
+vim.g.mapleader = ","
+
+-------------------------------------------------------------------------------
+-- Colors
+-------------------------------------------------------------------------------
+
+vim.cmd("set termguicolors")
+vim.cmd("set colorcolumn=100")
+vim.cmd("highlight LineNr guifg=#657b83")
+vim.cmd("highlight DiagnosticError guifg=#dc322f")
+vim.cmd("highlight DiagnosticWarn guifg=#b58900")
+vim.cmd("highlight DiagnosticUnderlineWarn guibg=#073642 cterm=none")
+vim.cmd("highlight DiagnosticHint guifg=#2aa198")
+vim.cmd("highlight DiagnosticUnderlineHint guibg=#073642 cterm=none")
+vim.cmd("sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=")
+vim.cmd("sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=")
+vim.cmd("sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=")
+vim.cmd("highlight DapStopped guibg=#073642")
+vim.cmd("highlight DapBreakpoint guifg=#dc322f")
+
+-------------------------------------------------------------------------------
+-- Plugins
+-------------------------------------------------------------------------------
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"christoomey/vim-tmux-navigator", -- Interop with tmux pane switching
+	"tpope/vim-fugitive", -- Git integration
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
-		tag = "release",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	})
-
-	-- Extended functionality
-	use("farmergreg/vim-lastplace")
-	use("tpope/vim-surround")
-
-	use({
-		"AckslD/nvim-neoclip.lua",
-		requires = {
-			{ "nvim-telescope/telescope.nvim" },
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
 		},
-		config = function()
-			require("neoclip").setup()
-		end,
-	})
+	},
+	"farmergreg/vim-lastplace", -- Persist cursor position
+	"tpope/vim-surround", -- Additional text objects
 
-	-- UI
-	use({
+	{
 		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local actions = require("telescope.actions")
 
@@ -82,11 +114,12 @@ require("packer").startup(function(use)
 				},
 			})
 		end,
-	})
-	use("rrethy/nvim-base16")
-	use({
+	},
+
+	"rrethy/nvim-base16",
+	{
 		"feline-nvim/feline.nvim",
-		requires = { { "kyazdani42/nvim-web-devicons" } },
+		dependencies = { { "kyazdani42/nvim-web-devicons" } },
 		config = function()
 			local solarized = {
 				fg = "#abb2bf",
@@ -211,38 +244,45 @@ require("packer").startup(function(use)
 				},
 			})
 		end,
-	})
-	use("dstein64/nvim-scrollview")
+	},
+	"dstein64/nvim-scrollview",
 
-	-- LSP / Completion
-	use("neovim/nvim-lspconfig", {
-		opts = {
-			format = { timeout_ms = 10000 },
-		},
-	})
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
+	-- LSP
+	"neovim/nvim-lspconfig",
+
+	-- Linting
+	{
+		"mfussenegger/nvim-lint",
 		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.code_actions.gitsigns,
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.gofmt,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.diagnostics.eslint,
-					null_ls.builtins.diagnostics.ansiblelint,
-					null_ls.builtins.diagnostics.rubocop,
-					null_ls.builtins.completion.tags,
+			require("lint").linters_by_ft = {
+				markdown = { "vale" },
+				ansible = { "ansible_lint" },
+				javascript = { { "eslint_d", "eslint" } },
+				typescript = { { "eslint_d", "eslint" } },
+			}
+		end,
+	},
+
+	-- Formatting
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "isort", "black" },
+					javascript = { { "prettierd", "prettier" } },
+					typescript = { { "prettierd", "prettier" } },
+					go = { "gofmt" },
 				},
 			})
 		end,
-	})
-	use({
+	},
+
+	-- Completion
+	{
 		"hrsh7th/nvim-cmp",
-		requires = { { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-vsnip", "hrsh7th/vim-vsnip" } },
+		dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-vsnip", "hrsh7th/vim-vsnip" },
 		config = function()
 			-- Configure LSP Servers
 			local cmp = require("cmp")
@@ -309,83 +349,8 @@ require("packer").startup(function(use)
 				},
 			})
 		end,
-	})
-
-	-- Debugging
-	use({
-		"mfussenegger/nvim-dap",
-		config = function()
-			local dap = require("dap")
-
-			dap.defaults.fallback.external_terminal = {
-				command = "tmux",
-				args = { "split-pane" },
-			}
-			dap.adapters.python = {
-				type = "executable",
-				command = "/usr/local/bin/python3",
-				args = { "-m", "debugpy.adapter" },
-			}
-			dap.configurations.python = {
-				{
-					type = "python",
-					request = "launch",
-					name = "Launch file",
-					program = "${file}",
-					pythonPath = function()
-						return "/usr/local/bin/python3"
-					end,
-					args = { "-m", "debugpy.adapter" },
-				},
-			}
-		end,
-	})
-	use({
-		"rcarriga/nvim-dap-ui",
-		requires = { "mfussenegger/nvim-dap" },
-		config = function()
-			require("dapui").setup({})
-		end,
-	})
-	use({
-		"theHamsta/nvim-dap-virtual-text",
-		config = function()
-			require("nvim-dap-virtual-text").setup({
-				enabled = true, -- enable this plugin (the default)
-				enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-				highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-				highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-				show_stop_reason = true, -- show stop reason when stopped for exceptions
-				commented = true, -- prefix virtual text with comment string
-				only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
-				all_references = true, -- show virtual text on all all references of the variable (not only definitions)
-				clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
-				--- A callback that determines how a variable is displayed or whether it should be omitted
-				--- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
-				--- @param buf number
-				--- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
-				--- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
-				--- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
-				--- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
-				display_callback = function(variable, buf, stackframe, node, options)
-					if options.virt_text_pos == "inline" then
-						return " = " .. variable.value
-					else
-						return variable.name .. " = " .. variable.value
-					end
-				end,
-				-- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
-				virt_text_pos = "eol",
-
-				-- experimental features:
-				all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-				virt_lines = true, -- show virtual lines instead of virtual text (will flicker!)
-				virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
-				-- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
-			})
-		end,
-	})
-end)
+	},
+})
 
 vim.diagnostic.config({
 	severity_sort = true,
@@ -399,74 +364,30 @@ vim.diagnostic.config({
 
 vim.o.updatetime = 250
 vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})]])
-
------------------------------------------------------------
--- Settings
------------------------------------------------------------
-
-vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.number = true
-vim.opt.signcolumn = "number"
-vim.opt.showmatch = true
-vim.opt.undofile = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.scrolloff = 4
-
------------------------------------------------------------
--- Colors
------------------------------------------------------------
-
-vim.cmd("set termguicolors")
-vim.cmd("set colorcolumn=100")
 vim.cmd("colorscheme base16-solarized-dark")
-vim.cmd("highlight LineNr guifg=#657b83")
-vim.cmd("highlight DiagnosticError guifg=#dc322f")
-vim.cmd("highlight DiagnosticWarn guifg=#b58900")
-vim.cmd("highlight DiagnosticUnderlineWarn guibg=#073642 cterm=none")
-vim.cmd("highlight DiagnosticHint guifg=#2aa198")
-vim.cmd("highlight DiagnosticUnderlineHint guibg=#073642 cterm=none")
-vim.cmd("sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=")
-vim.cmd("sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=")
-vim.cmd("sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=")
-vim.cmd("highlight DapStopped guibg=#073642")
-vim.cmd("highlight DapBreakpoint guifg=#dc322f")
 
-local sign = vim.fn.sign_define
-
-sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "DapBreakpoint" })
-sign("DapStopped", { texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
-
------------------------------------------------------------
--- Key mappings
------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- Keymappings
+-------------------------------------------------------------------------------
 
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
 vim.keymap.set("n", ";", ":")
-vim.keymap.set("n", "<C-c>", "<cmd>noh<CR>")
+vim.keymap.set("n", "<C-C>", "<cmd>nohlsearch<CR>")
 
 local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", telescope.find_files)
 vim.keymap.set("n", "<C-f>", telescope.live_grep)
-vim.keymap.set("n", "<C-e>", "<cmd>TroubleToggle<CR>")
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
--- Debugging
 vim.keymap.set("n", "<space>", function()
 	require("dap").continue()
 end)
 
------------------------------------------------------------
--- Leader mappings
------------------------------------------------------------
-
-vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>r", ":source $MYVIMRC<CR>")
 vim.keymap.set("n", "<leader>p", ":PackerSync<CR>")
 vim.keymap.set("n", "<leader>f", function()
-	vim.lsp.buf.format({ timeout_ms = 20000 })
+	require("conform").format()
 end)
 vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
 vim.keymap.set("v", "<leader>c", '"+y')
